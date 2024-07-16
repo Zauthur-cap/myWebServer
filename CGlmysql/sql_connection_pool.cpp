674764d7e0e93c7bf278e21405b9d7955613aeac
Zauthur-cap <1322497995@qq.com>
2024-07-16 23:35:50 +0800

    day3

---
+++ CGlmysql/sql_connection_pool.cpp
@@ -0,0 +1,23 @@
+﻿#include <mysql/mysql.h>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <list>
+#include <pthread.h>
+#include <iostream>
+#include "sql_connection_pool.h"
+
+using namespace std;
+
+connection_pool::connection_pool(){
+    m_CurConn = 0;
+    m_FreeConn = 0;
+}
+connection_pool *connection_pool::Getinstance(){
+    static connection_pool connPool;
+    return &connPool;
+}
+connection_pool::~connection_pool(){
+    DestroyPool();
+}
